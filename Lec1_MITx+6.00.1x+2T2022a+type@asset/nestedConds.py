b61df2176ca65d7fec89d569e9bd87ecf44fa172
eribertto_at_nixOS <erimendz@gmail.com>
2023-02-11 17:04:41 +0300

    add missing variable x

--- Lec1_MITx+6.00.1x+2T2022a+type@asset/nestedConds.py
+++ Lec1_MITx+6.00.1x+2T2022a+type@asset/nestedConds.py
@@ -4,7 +4,7 @@ Created on Wed Jun  8 11:07:33 2016
 
 @author: ericgrimson
 """
-
+x = int(input("Enter an integer number: "))
 if x%2 == 0:
     if x%3 == 0:
         print('Divisible by 2 and 3')
@@ -12,3 +12,5 @@ if x%2 == 0:
         print('Divisible by 2 and not by 3')
 elif x%3 == 0:
     print('Divisible by 3 and not by 2')
+else:
+    print(f'The number is {x}')
